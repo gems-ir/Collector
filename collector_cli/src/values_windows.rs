f9877bf5a11a04810d1aef9fb44e9f51c6b040cb
tranosgames <pubtranos@gmail.com>
2025-11-09 22:01:22 +0100

    Available to input config file

--- collector_cli/src/values_windows.rs
+++ collector_cli/src/values_windows.rs
@@ -1,8 +1,7 @@
+pub const SOURCE_PATH: &str = "C:\\";
 
-pub const SOURCE_PATH: &str ="C:\\";
+pub const DESTINATION_PATH: &str = "output\\";
 
-pub const DESTINATION_PATH: &str ="output\\";
-
-pub const RESOURCES_EXEMPLE: &str ="All";
+pub const RESOURCES_EXAMPLE: &str = "All";
 
 pub const PATH_RESOURCE: &str = "Resources\\";
